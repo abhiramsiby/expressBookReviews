83d432eb38cd4b6fdacbfe81d94a0c77a50fa0e5
Abhiram Siby <abhiramsibi2@gmail.com>
2026-06-21 14:09:51 +0530

    Added get full book details and get details based on isbn

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -7,19 +7,30 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
+
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  let booksDetails=JSON.stringify(books);
+  return res.status(300).json({message: "Available Books are\n" +booksDetails});
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn=parseInt(req.params.isbn);
+  const book=books[isbn];
+  if(book){
+    booksDetails=JSON.stringify(book,null,4);
+    return res.status(300).json({message:"The book Details is\n" +booksDetails});
+  }
+  else{
+    return res.status(300).json({message: "No book found"});
+
+  }
  });
   
 // Get book details based on author
